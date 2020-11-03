d23dcdef5d876f306ef991930d2ae49e30de8357
Spina <alonzopina7@gmail.com>
2020-11-03 00:02:00 -0600

    tabla-creada

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React from 'react';
 import './Dashboard.css';
 import * as MdIcons from "react-icons/md";
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+
 
 function Dashboard() {
+  const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  });
+  
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+  
+  const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+  ];
+  
+    const classes = useStyles();
     return (
         <div className='container'>
         <div className='dashboard'>
@@ -52,31 +81,34 @@ function Dashboard() {
 
         </div>
         <div className='dashboard__5'>
-            <div className='dashboard__8'>
-            <p id='p1_dashboard'>Tracking</p>
-            <p id='p2_dashboard'>Detalles</p>
-            <p id='p2_dashboard'>Estado</p>
-            <p id='p2_dashboard'>Fecha</p>
-            <p id='p2_dashboard'>Acciones</p>
-            </div>
-        <div className='dashboard__6'>
-        <div className='dashboard__7'>
-        <p id='p4'>234234</p>
-            <p id='p3'>Sender: Anyád szűz</p>
-            <p id='p3'>Recibido</p>
-            <p id='p3'>Nov 01, 2020</p>
-</div>
-<div className='dashboard__7'>
-
-</div>
-<div className='dashboard__7'>
-
-</div>
-<div className='dashboard__7'>
-
-</div>
+        <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
         </div>
-        </div>
+        
         </div>
     )
 }
